c9e88b10ebd8159652e6886ba78a3ae1f63a2b0e
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2024-12-23 08:56:17 -0500

    KernelSU: Upgrade @rifsxd fork (KernelSU Next) to v1.0.2-R12

--- drivers/kernelsu/ksu.c
+++ drivers/kernelsu/ksu.c
@@ -3,7 +3,6 @@
 #include <linux/kobject.h>
 #include <linux/module.h>
 #include <linux/workqueue.h>
-#include "linux/init.h"
 
 #include "allowlist.h"
 #include "arch.h"
@@ -15,19 +14,6 @@
 #ifdef CONFIG_KSU_SUSFS
 #include <linux/susfs.h>
 #endif
-
-unsigned int enable_kernelsu = 1;
-static int __init read_kernelsu_state(char *s)
-{
-	if (s)
-		enable_kernelsu = simple_strtoul(s, NULL, 0);
-	return 1;
-}
-__setup("kernelsu.enabled=", read_kernelsu_state);
-unsigned int get_ksu_state(void)
-{
-	return enable_kernelsu;
-}
 
 static struct workqueue_struct *ksu_workqueue;
 
@@ -57,11 +43,6 @@ extern void ksu_ksud_exit();
 
 int __init ksu_kernelsu_init(void)
 {
-	if (enable_kernelsu < 1) {
-		pr_info_once(" is disabled");
-		return 0;
-	}
-
 #ifdef CONFIG_KSU_DEBUG
 	pr_alert("*************************************************************");
 	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
@@ -101,9 +82,6 @@ int __init ksu_kernelsu_init(void)
 
 void ksu_kernelsu_exit(void)
 {
-	if (enable_kernelsu < 1)
-		return;
-	
 	ksu_allowlist_exit();
 
 	ksu_throne_tracker_exit();
